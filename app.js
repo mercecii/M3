dad0e92a1efc2aea1b61c7d7ae71f0955df2438d
mercecii <deepak986698@gmail.com>
2020-09-22 13:54:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const request = require('request');
 
 const app = express();
-const port = 5400;
+const port = process.env.port;
 
 // var url = "http://api.openweathermap.org/data/2.5/weather?q=patna&appid=53508d3c7785dcc45e3152283b14294a"
 var url = "http://api.openweathermap.org/data/2.5/forecast?q=patna&units=metric&appid=53508d3c7785dcc45e3152283b14294a"
